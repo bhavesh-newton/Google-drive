4324fd2c6c4f428dc532359a6b4e1732ebccccc3
bhavesh-newton <bhavesh.bansal@newtonschool.co>
2024-09-02 16:00:13 +0530

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const dotenv = require("dotenv");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { connectDB, prismaClient } = require("./config/database");
+const port = 3000;
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+app.post('/signup', async (req, res) => {
+    const user = req.body;
+    const password = await bcrypt.hash(user.password, 10);
+    user.password = password;
+    try {
+        await prismaClient.users.create({
+            data: user
+        });
+        res.send('signup successful')
+    } catch (err) {
+        res.status(400).send('User already exists')
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await prismaClient.users.findUnique({
+        where: {
+            email: email
+        }
+    });
+    if (user) {
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (isPasswordCorrect) {
+            const payload = {
+                name: user.name,
+                email
+            };
+            const secret = process.env.JWT_SECRET;
+            const token = jwt.sign(payload, secret, {
+                expiresIn: '24h'
+            })
+            res.send({ token })
+        } else {
+            res.status(401).send('Wrong password.')
+        }
+    } else {
+        res.status(404).send("User doesn't exist.")
+    }
+})
+
+app.get('/user/:userId', async (req, res) => {
+    const token = req.headers.authorization;
+    try {
+        jwt.verify(token, process.env.JWT_SECRET);
+        const { userId } = req.params;
+        const user = await prismaClient.users.findUnique({
+            where: {
+                id: Number(userId)
+            }
+        })
+        res.send({
+            id: userId,
+            name: user.name,
+            email: user.email
+        })
+    } catch(err) {
+        res.status(401).send('Unauthorized user.')
+    }
+})
+
+app.get('/files-and-folders/*', (req, res) => {
+    const subPath = req.params[0];
+    const folderPath = './shared/' + subPath
+    const filesAndFolders = fs.readdirSync(folderPath);
+    const result = []
+    for (const name of filesAndFolders) {
+        const isFolder = fs.statSync(folderPath + "/" + name).isDirectory();
+        const data = {
+            name,
+            type: isFolder ? 'folder' : 'file'
+        }
+        result.push(data)
+    }
+    res.send(result)
+})
+
+app.get('/file/*', (req, res) => {
+    const subPath = req.params[0];
+    const filePath = './shared/' + subPath;
+    if (fs.existsSync(filePath)) {
+        const absolutePath = path.resolve(__dirname, filePath);
+        res.sendFile(absolutePath);
+    } else {
+        res.status(404).send('File not found');
+    }
+})
+
+app.post('/file/*', (req, res) => {
+    const subPath = req.params[0];
+    const filePath = './shared/' + subPath;
+    if (fs.existsSync(filePath)) {
+        res.status(400).send('File already exist.');
+    } else {
+        const content = req.body;
+        fs.writeFileSync(filePath, JSON.stringify(content, null, 2));
+        res.send('File created successfully.')
+    }
+})
+
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+});
